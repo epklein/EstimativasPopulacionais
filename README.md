b562817d4f8e7b92519d02f71915204936667887
epklein@gmail.com <Eduardo Klein>
2017-11-05 00:50:15 -0200

    Construção do documento RMarkdown

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # EstimativasPopulacionais-Brasil-2017-IBGE
-Projeto desenvolvido em R para explorar as estimativas populacionais do Brasil, utilizando dados do IBGE.
 
-Este é o projeto RStudio base para construção das análises e gráficos publicados em https://decifrandodados.wordpress.com/2017/10/11/brasil-e-suas-estimativas-populacionais-2017/
+Projeto desenvolvido em R para explorar as estimativas populacionais do Brasil, utilizando dados do IBGE do ano de 2017.
+
+* Estimativas populacionais de 2017: https://www.ibge.gov.br/estatisticas-novoportal/sociais/populacao/9103-estimativas-de-populacao.html
+* Área territorial do Brasil, Regiões e Unidades Federativas: https://ww2.ibge.gov.br/home/geociencias/areaterritorial/principal.shtm
+
+Este é o projeto RStudio base para construção das análises e gráficos publicados em https://web.decifrandodados.com.br/EstimativasPopulacionais-Brasil
---
+++ global.R
@@ -0,0 +1,80 @@
+library(readxl)
+
+countryName <- c("Brasil")
+regionName <- c("Região Norte", "Região Nordeste", "Região Sudeste",
+                "Região Sul", "Região Centro-Oeste")
+
+# ----- FUNÇÕES DE IMPORTAÇÃO DE DADOS -----
+readPopulationData <- function() {
+  
+  # carga das estimativas populacionais e formatação
+  # uso da biblioteca readxl para leitura diretamente de planilha Excel
+  rawDataPop <- read_excel("estimativa_dou_2017.xls",
+                           sheet <- "BRASIL E UFs",
+                           range="A2:C35")
+  
+  # exclusão de coluna vazia e acerto dos nomes das colunas
+  rawDataPop$X__1 <- NULL
+  colnames(rawDataPop) <- c("Unit", "Population")
+  
+  # limpeza e formatação da informação de População
+  rawDataPop$Population <- gsub(" \\(\\*\\)", "", rawDataPop$Population)
+  rawDataPop$Population <- gsub(" \\(\\*\\*\\)", "", rawDataPop$Population)
+  rawDataPop$Population <- gsub("\\.", "", rawDataPop$Population)
+  
+  rawDataPop$Population <- as.integer(rawDataPop$Population)
+
+  #formatação da tabela final de populações
+  
+  dataPop <- NULL
+  
+  #região Norte
+  tempDF <- rawDataPop[c(3:9),]
+  tempDF$Region <- regionName[1]
+  dataPop <- rbind(dataPop, tempDF)
+  
+  #região Nordeste
+  tempDF <- rawDataPop[c(11:19),]
+  tempDF$Region <- regionName[2]
+  dataPop <- rbind(dataPop, tempDF)
+  
+  #região Sudeste
+  tempDF <- rawDataPop[c(21:24),]
+  tempDF$Region <- regionName[3]
+  dataPop <- rbind(dataPop, tempDF)
+  
+  #região Sul
+  tempDF <- rawDataPop[c(26:28),]
+  tempDF$Region <- regionName[4]
+  dataPop <- rbind(dataPop, tempDF)
+  
+  #região Centro-Oeste
+  tempDF <- rawDataPop[c(30:33),]
+  tempDF$Region <- regionName[5]
+  dataPop <- rbind(dataPop, tempDF)
+  
+  dataPop$Region <- factor(dataPop$Region, levels=regionName)
+  dataPop$Unit <- factor(dataPop$Unit)
+  
+  return (dataPop)
+}
+
+readAreaData <- function() {
+  
+  # carga das medidas de área das Unidades Federativas
+  dataArea <- read.csv2("area_UF_Brasil.csv", encoding="UTF-8")
+
+  # limpeza e formatação da informação de Área
+  dataArea$Área..Km2. <- gsub("\\.", "", dataArea$Área..Km2.)
+  dataArea$Área..Km2. <- gsub(",", "\\.", dataArea$Área..Km2.)
+  dataArea$Área..Km2. <- as.numeric(dataArea$Área..Km2.)
+  
+  # formatação da tabela final de áreas
+  dataArea$Código.UF <- NULL
+  colnames(dataArea) <- c("Unit", "Area")
+
+  return (dataArea)
+}
+
+dataPopulation <- readPopulationData()
+dataArea <- readAreaData()
